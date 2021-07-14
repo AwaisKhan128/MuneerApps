99ac39de91f7f643365120fdab3a314585136cbb
Awais <awais.khan128@yahoo.com>
2021-07-14 19:00:44 +0500

    v2

--- app/src/main/java/com/example/muneerapps/MainActivity.java
+++ app/src/main/java/com/example/muneerapps/MainActivity.java
@@ -6,30 +6,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewStub;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.auth0.android.Auth0;
-import com.auth0.android.Auth0Exception;
-import com.auth0.android.authentication.AuthenticationException;
-import com.auth0.android.callback.Callback;
-import com.auth0.android.provider.WebAuthProvider;
-import com.auth0.android.result.Credentials;
-
 import connection.Connection;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Context mContext;
+    WebView WebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = this;
-        ViewStub stub = (ViewStub) findViewById(R.id.layout_stub);
-        stub.setLayoutResource(R.layout.layout_login);
-        View inflated = stub.inflate();
+        WebView = findViewById(R.id.webView);
+        WebView.loadUrl(Connection.API+"/signup");
+        WebSettings webSettings = WebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        WebView.getSettings().setLoadWithOverviewMode(true);
+        WebView.getSettings().setUseWideViewPort(true);
+
 
     }
 
@@ -42,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     {
 
     }
+
 
 
 }
