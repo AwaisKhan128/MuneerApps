ecf46c381ee5fa8c5fe9f59ef355ff6a0e096672
Awais <awais.khan128@yahoo.com>
2021-07-23 16:40:03 +0500

    v2

--- app/src/main/java/com/example/muneerapps/Selector.java
+++ app/src/main/java/com/example/muneerapps/Selector.java
@@ -2,8 +2,13 @@ package com.example.muneerapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.muneerapps.dialogs.Category_dialog;
+import com.example.muneerapps.dialogs.Customer_dialog;
+import com.example.muneerapps.dialogs.Product_dialog;
 
 public class Selector extends AppCompatActivity {
 
@@ -15,22 +20,26 @@ public class Selector extends AppCompatActivity {
 
     public void create_custom(View view)
     {
+        Customer_dialog cdd=new Customer_dialog(Selector.this);
+        cdd.show();
 
     }
 
     public void create_categ(View view)
     {
-
+        Category_dialog cdd=new Category_dialog(Selector.this);
+        cdd.show();
     }
 
     public void create_pay(View view)
     {
-
+        startActivity(new Intent(Selector.this,Payment.class));
     }
 
     public void create_prod(View view)
     {
-
+        Product_dialog cdd=new Product_dialog(Selector.this);
+        cdd.show();
     }
 
 }
