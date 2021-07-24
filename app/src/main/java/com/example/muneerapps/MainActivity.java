564c39cc5daa34d80c333162f19712a82c1e9544
Awais <awais.khan128@yahoo.com>
2021-07-24 15:40:18 +0500

    v2

--- app/src/main/java/com/example/muneerapps/MainActivity.java
+++ app/src/main/java/com/example/muneerapps/MainActivity.java
@@ -4,24 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewStub;
 import android.view.Window;
 import android.view.WindowManager;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import connection.Connection;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     EditText userName, emails,password,password2;
     TextView signin;
     Button button3;
+    ProgressBar progressBar;
 
     public void signin_transact(View view)
     {
@@ -50,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password);
         password2 = (EditText) findViewById(R.id.password2);
         signin = (TextView) findViewById(R.id.signin);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        button3 = (Button) findViewById(R.id.button3);
+        progressBar.setVisibility(View.GONE);
     }
 
     public void setButton3(View view)
@@ -58,6 +59,28 @@ public class MainActivity extends AppCompatActivity {
         String email = emails.getText().toString();
         String password_u = password.getText().toString();
         String password2_u = password2.getText().toString();
+
+        if (password_u.compareToIgnoreCase(password2_u)==0 && password_u.length()>=6)
+        {
+            if (email.compareToIgnoreCase("")==0 && user.compareToIgnoreCase("")==0) {
+                return;
+            }
+            else
+            {
+                Register_firebase_user register_firebase_user = new Register_firebase_user();
+                register_firebase_user.execute(email, password_u);
+            }
+
+        }
+        else
+        {
+            Toaster("Passwords must match or check length > 6");
+            password.setError("Not Match");
+            password2.setError("Not Match");
+        }
+
+
+
 
     }
 
@@ -71,6 +94,71 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
+    public class Register_firebase_user extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+            button3.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            AtomicBoolean ans = new AtomicBoolean(false);
+//            if ( !password_u.equals("") && !password2_u.equals("") )
+            {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(strings[0], strings[1])
+                        .addOnCompleteListener(MainActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                Toaster("User Created Successfully");
+                                progressBar.setVisibility(View.GONE);
+                                button3.setVisibility(View.VISIBLE);
+                                ans.set(true);
+
+                            } else {
+                                Toaster(task.getException().getMessage());
+                                progressBar.setVisibility(View.GONE);
+                                button3.setVisibility(View.VISIBLE);
+                                ans.set(false);
+                            }
+                        });
+            }
+            if (ans.get())
+            {
+                return "Success";
+            }
+            else
+            {
+                return "Failed";
+            }
+
+
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/muneerapps/signin.java
+++ app/src/main/java/com/example/muneerapps/signin.java
@@ -1,18 +1,35 @@
 package com.example.muneerapps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import connection.Connection;
 
 public class signin extends AppCompatActivity {
     Context mContext;
+    ProgressBar progressBar2;
+    Button button13;
+    EditText email2,password3;
 
 
     @Override
@@ -20,12 +37,101 @@ public class signin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
         mContext = this;
-
+        progressBar2 = (ProgressBar) findViewById(R.id.progressBar2);
+        button13 = (Button) findViewById(R.id.button13);
+        email2 = (EditText) findViewById(R.id.email2);
+        password3 = (EditText) findViewById(R.id.password3);
+        progressBar2.setVisibility(View.GONE);
     }
-
 
     public void go_forward(View view)
     {
         startActivity(new Intent(this,Selector.class));
+    }
+
+    public void SignIN(View view)
+    {
+        if (email2.getText().toString().length()>0 && password3.getText().toString().length()>=6)
+        {
+            Sign_In sign_in = new Sign_In();
+            sign_in.execute(email2.getText().toString(),password3.getText().toString());
+
+        }
+    }
+
+    public class Sign_In extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar2.setVisibility(View.VISIBLE);
+            button13.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            AtomicBoolean ans = new AtomicBoolean(false);
+//            if ( !password_u.equals("") && !password2_u.equals("") )
+            {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(strings[0],strings[1])
+                        .addOnCompleteListener(signin.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful())
+                                {
+                                    Toaster("User Sign In Successfully");
+                                    progressBar2.setVisibility(View.GONE);
+                                    button13.setVisibility(View.VISIBLE);
+                                    startActivity(new Intent(signin.this,Selector.class));
+
+                                }
+                                else
+                                {
+                                    Toaster("User Sign In Failed due to "+task.getException().getLocalizedMessage());
+                                    progressBar2.setVisibility(View.GONE);
+                                    button13.setVisibility(View.VISIBLE);
+                                }
+                            }
+                        });
+            }
+            if (ans.get())
+            {
+                return "Success";
+            }
+            else
+            {
+                return "Failed";
+            }
+
+
+        }
+    }
+
+    public void Toaster(String s)
+    {
+        Toast.makeText(mContext,s,Toast.LENGTH_SHORT).show();
     }
 }
