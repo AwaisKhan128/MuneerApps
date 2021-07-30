9e1a0e3f5a7f5396a44a5bc6d7442f8992a7144e
Awais <awais.khan128@yahoo.com>
2021-07-31 00:38:30 +0500

    v2

--- app/src/main/java/com/example/muneerapps/Transactions/Transaction_Adaptor.java
+++ app/src/main/java/com/example/muneerapps/Transactions/Transaction_Adaptor.java
@@ -1,11 +1,14 @@
 package com.example.muneerapps.Transactions;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.muneerapps.R;
 
 import java.util.ArrayList;
 
@@ -22,19 +25,43 @@ public class Transaction_Adaptor extends RecyclerView.Adapter<Transaction_Holder
         this.data = data;
     }
 
-    @NonNull
     @Override
-    public Transaction_Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Transaction_Holder holder, int position) {
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    @NonNull
+    @Override
+    public Transaction_Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.transactions, parent,false);
+        transaction_holder = new Transaction_Holder(view);
+        return transaction_holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Transaction_Holder holder, int i) {
+        holder.Order_title.setText(data.get(i).getOrder_title());
+        holder.Customer.setText(data.get(i).getCustomer());
+        holder.Categories.setText(data.get(i).getCategory());
+        holder.Products.setText(data.get(i).getProduct());
+        holder.Rates.setText(data.get(i).getRate());
+        holder.User.setText(data.get(i).getUser());
+        holder.Quantity.setText(data.get(i).getQuantity());
+        holder.Amount.setText(data.get(i).getAmount());
+        holder.Amount.setTextColor(data.get(i).getAmount_color());
+        holder.Date_time.setText(data.get(i).getDate_time());
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 }
